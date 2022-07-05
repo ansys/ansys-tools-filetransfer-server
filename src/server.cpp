3560515df3c8e5915f7484b25f5e396d01afe107
Dominik Gresch <dominik.gresch@ansys.com>
2022-07-05 23:09:55 +0200

    Add partial CI file

--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <ansys/api/utilities/filetransfer/v1/file_transfer_service.grpc.pb.h>
+#include <iostream>
 
 int main(int argc, char** argv) {
     std::cout << "HELLO WORLD" << std::endl;
